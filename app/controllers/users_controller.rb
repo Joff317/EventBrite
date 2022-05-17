f315dfcb883c94cd694c6066493d3f2a3ff8108b
Joff317 <j.freixanet@hotmail.com>
2022-05-17 13:50:52 +0200

    last commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
    before_action :set_user, only: %i[ show edit update destroy ]
+   before_action :authenticate_user!, only: [:show, :edit, :update, :destroy]
+   
 
    def index 
       @users = User.all
    end
+
+   def show 
+      @events = Event.all 
+   end
+
 
    private
     # Use callbacks to share common setup or constraints between actions.
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,7 @@
 module EventsHelper
+
+   def get_end_date(event)
+      @event.start_date + @event.duration*60
+    end
+    
 end
